a212edcf0dc0cef14158f683e5f4e88a7ebf9123
KaeL <uberkael@gmail.com>
2025-07-14 18:22:29 +0200

    Add Config

---
+++ config.py
@@ -0,0 +1,24 @@
+import dspy  # type: ignore
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+google_key = os.getenv("GOOGLE_API_KEY")
+
+# Disable cache for the LM
+# dspy.configure_cache(
+# 	enable_disk_cache=False,
+# 	enable_memory_cache=False,
+# )
+
+
+# lm = dspy.LM(model='ollama/qwen3')
+# lm = dspy.LM(model='ollama/deepseek-r1')
+# lm = dspy.LM(model='ollama/deepseek-v2')
+# lm = dspy.LM(model='ollama/mistral')
+# lm = dspy.LM(model='ollama/gemma3')
+# lm = dspy.LM('gemini/gemini-2.5-pro', api_key=google_key)
+lm = dspy.LM('gemini/gemini-2.5-flash-lite-preview-06-17', api_key=google_key)
+
+dspy.configure(lm=lm)
