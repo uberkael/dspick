ed136d1b0beccb9f26b4d98f917e9655200cef0d
KaeL <uberkael@gmail.com>
2025-07-15 16:55:42 +0200

    Cleanup

--- a.bash
+++
@@ -1,9 +0,0 @@
-__mi_widget() {
-	local selected=$(./a.py)
-	if [[ -n "$selected" ]]; then
-		READLINE_LINE="$selected"
-		READLINE_POINT=${#READLINE_LINE}
-	fi
-}
-
-bind -x '"\C-g":__mi_widget'
--- a.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-selected=$(echo -e "git status\ndocker ps\ngrep TODO *" | fzf --prompt="Comando: ")
-
-if [[ -n "$selected" ]]; then
-	printf "%s" "$selected"
-fi
