579cf84107e848be6053e504fae2697fd68518c8
KaeL <uberkael@gmail.com>
2025-07-19 00:10:48 +0200

    Prediction Add optimized predictor conditional loading

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -1,4 +1,4 @@
-# %%
+import os
 import dspy  # type: ignore
 import platform
 from config.config import lm
@@ -8,7 +8,15 @@ from signature import DescriptionCommand
 _ = lm
 
 
-def predict(q: str = "list all files and hidden files in the current directory") -> DescriptionCommand:
+def predict(
+	q: str = "list all files and hidden files in the current directory",
+	path = "optimized.pkl") -> DescriptionCommand:
 	"""Predict the command to execute based on the description."""
-	p = dspy.Predict(DescriptionCommand)
-	return p(description=f"in a operating system: {platform.system()}, what is the command and arguments to do: {q}")
+	d = f"in a operating system: {platform.system()}, what is the command and arguments to do: {q}"
+	if os.path.exists(path):
+		p = dspy.Predict(DescriptionCommand)
+		p.load(path)
+		return p(description=d)
+	else:
+		p = dspy.Predict(DescriptionCommand)
+		return p(description=d)
